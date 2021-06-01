c4e697fc5d4a0c94efeb621b18cd09f7f625c22b
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-06-01 06:53:52 -0500

    added hours page

--- dist/scripts/main.js
+++
@@ -1,5 +0,0 @@
-function hoursBox() {
-  alert(
-    "HOURS OF OPERATIONS:\n\nMon:  10:30am - 7pm\nTue:   10:30am - 7pm\nWed:  10:30am - 7pm\nThu:   10:30am - 7pm\nFri:     10:30am - 7pm\nSat:    10:30am - 6pm\nSun:    CLOSED"
-  );
-}
